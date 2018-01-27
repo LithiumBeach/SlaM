48417a9739b497346640e56c41b7141750feaa38
nicholaspedalino <njpedalino@gmail.com>
2018-01-27 00:34:55 -0500

    pushers party

---
+++ AndWelcomeToTheJam/Assets/Code/Piston.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Piston : MonoBehaviour, IComparable
+{
+    public Rigidbody2D m_Pusher = null;
+
+    public void Awake()
+    {
+        PistonManager.Instance.AddPiston(this);
+    }
+
+    public int CompareTo(object _rhs)
+    {
+        Piston rhs = _rhs as Piston;
+        Debug.Assert(rhs != null);
+        return transform.IsChildOf(rhs.transform) ? 1 : -1;
+    }
+
+    public void ExtendPusher()
+    {
+        m_Pusher.transform.position += m_Pusher.transform.up;
+    }
+
+    public void RetractPusher()
+    {
+        //m_Pusher.MovePosition(m_Pusher.transform.position + -m_Pusher.transform.up);
+        m_Pusher.transform.position -= m_Pusher.transform.up;
+    }
+}
---
+++ AndWelcomeToTheJam/Assets/Code/PistonManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using patterns;
+using System;
+
+public class PistonManager : SingletonBehavior<PistonManager>
+{
+    public List<Piston> m_Pistons = new List<Piston>();
+    public void AddPiston(Piston p) { m_Pistons.Add(p); m_Pistons.Sort(); }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ExtendPushers();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            RetractPushers();
+        }
+    }
+
+    private void ExtendPushers()
+    {
+        for (int i = 0; i < m_Pistons.Count; i++)
+        {
+            StartCoroutine("ExtendPusherDelayed", new object[] { .1f * i, m_Pistons[i] });
+        }
+        //m_Pistons.ForEach(item => item.ExtendPusher());
+    }
+    private IEnumerator ExtendPusherDelayed(object[] o)
+    {
+        yield return new WaitForSeconds((float)o[0]);
+        (o[1] as Piston).ExtendPusher();
+    }
+
+    private void RetractPushers()
+    {
+        for (int i = m_Pistons.Count-1; i >= 0; i--)
+        {
+            StartCoroutine("RetractPusherDelayed", new object[] { .1f * i, m_Pistons[m_Pistons.Count - 1 - i] });
+        }
+        //m_Pistons.ForEach(item => item.RetractPusher());
+    }
+    private IEnumerator RetractPusherDelayed(object[] o)
+    {
+        yield return new WaitForSeconds((float)o[0]);
+        (o[1] as Piston).RetractPusher();
+    }
+
+}
