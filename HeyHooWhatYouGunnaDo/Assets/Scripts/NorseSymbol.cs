56d7f0c009c0d3169093e141f1c2897fce1717ee
nicholaspedalino <njpedalino@gmail.com>
2018-01-28 10:45:29 -0500

    colors

--- HeyHooWhatYouGunnaDo/Assets/Scripts/NorseSymbol.cs
+++ HeyHooWhatYouGunnaDo/Assets/Scripts/NorseSymbol.cs
@@ -16,6 +16,10 @@ public class NorseSymbol : MonoBehaviour
     [SerializeField]
     public List<NorseLine> m_Lines = new List<NorseLine>();
 
+
+    public Color m_DotsColor = Color.black;
+    public Gradient m_LinesColorGradient;
+
     public float m_DotSpacingScale = .25f;
 
     private void OnDotActivate(NorseDot dot)
@@ -44,6 +48,7 @@ public class NorseSymbol : MonoBehaviour
                     //dot.m_LineRenderer.SetPositions(new Vector3[] { dot.transform.position, prevDot.transform.position });
                     NorseLine nl = new NorseLine();
                     LineRenderer l = Instantiate(PrefabManager.Instance.m_Data.m_DotConnectLR.gameObject, transform).GetComponent<LineRenderer>();
+                    l.colorGradient = m_LinesColorGradient;
                     nl.line = l;
                     nl.A = prevDot;
                     nl.B = dot;
@@ -220,6 +225,7 @@ public class NorseSymbol : MonoBehaviour
                 iter.m_GridPositionY = y;
                 iter.AOnActivate = OnDotActivate;
                 iter.AOnDeactivate = OnDotDeactivate;
+                iter.m_SprDot.color = m_DotsColor;
 
                 //m_Dots[x][y] = iter;
                 m_Dots.Add(iter);
